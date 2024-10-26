2b950ed6f1a0ca0b91c9fa8a01b9f351c63d6028
Limehello <plxsmazis@gmail.com>
2024-10-27 00:53:46 +0800

    Update Main.java

--- com/github/jecko/Main.java
+++ com/github/jecko/Main.java
@@ -2,6 +2,6 @@ package com.github.jecko;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World! St");
     }
 }
