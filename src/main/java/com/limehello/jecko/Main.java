1f88df338a2052a84e9fc595199009535cf2d5a6
Limehello <plxsmazis@gmail.com>
2024-10-29 20:33:08 +0800

    Update Main.java

--- src/main/java/com/limehello/jecko/Main.java
+++ src/main/java/com/limehello/jecko/Main.java
@@ -1,7 +1,19 @@
 package com.limehello.jecko;
 
+import com.limehello.jecko.type.text.csv.CSVToken;
+import com.limehello.jecko.type.text.csv.CSVTokenType;
+import com.limehello.jecko.type.text.csv.CSVLexer;
+import com.limehello.jecko.type.text.csv.CSVTokens;
+
+import java.util.List;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello, World");
+    String csvInput = "\"John Doe\",29,\"New York\",\"He said, \"\"Hello\"\"\",true, \"Extra, comma\"";
+    CSVLexer lexer = new CSVLexer(csvInput);
+    List<CSVToken> tokens = lexer.tokenize();
+    for (CSVToken token : tokens) {
+      System.out.println(token);
+    }
   }
 }
