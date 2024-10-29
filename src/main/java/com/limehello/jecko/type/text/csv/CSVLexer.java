6797a7cc17223511519fa645362791f92d9f37d6
Limehello <plxsmazis@gmail.com>
2024-10-29 20:20:26 +0800

    Update CSVLexer.java

--- src/main/java/com/limehello/jecko/type/text/csv/CSVLexer.java
+++ src/main/java/com/limehello/jecko/type/text/csv/CSVLexer.java
@@ -6,7 +6,7 @@ import com.limehello.jecko.type.text.csv.CSVToken;
 import com.limehello.jecko.type.text.csv.CSVTokenType;
 
 public class CSVLexer {
-  private String input;
+  private final String input;
   private int position;
   public CSVLexer(String input) {
     this.input = input;
@@ -16,5 +16,70 @@ public class CSVLexer {
     List<CSVToken> tokens = new ArrayList<>();
     StringBuilder currentToken = new StringBuilder();
     CSVTokenType currentType = CSVTokenType.SPACE;
+    while (position < input.length()) {
+      char currentChar = input.charAt(position);
+      if (Character.isWhitespace(currentChar)) {
+        position++;
+        continue;
+      }
+      if (currentChar == ',') {
+        if (currentToken.length() > 0) {
+          tokens.add(createToken(currentToken.toString(), currentType));
+          currentToken.setLength(0);
+        }
+        tokens.add(new CSVToken(",", CSVTokenType.COMMA));
+        position++;
+        continue;
+      }
+      if (currentChar == '"') {
+        position++;
+        boolean isEscaped = false;
+        while (position < input.length()) {
+          currentChar = input.charAt(position);
+          if (currentChar == '"' && !isEscaped) {
+            if (position + 1 < input.length() && input.charAt(position + 1) == '"') {
+              currentToken.append('"');
+              isEscaped = true;
+              position += 2;
+            } else {
+              position++;
+              break;
+            }
+          } else {
+            currentToken.append(currentChar);
+            isEscaped = false;
+            position++;
+          }
+        }
+        tokens.add(new CSVToken(currentToken.toString(), CSVTokenType.QUOTED_STRING));
+        currentToken.setLength(0);
+        continue;
+      }
+      if (Character.isDigit(currentChar) || 
+          (currentChar == '-' && position + 1 < input.length() && 
+           Character.isDigit(input.charAt(position + 1)))) {
+        while (position < input.length() && 
+              (Character.isDigit(input.charAt(position)) || input.charAt(position) == '.')) {
+          currentToken.append(input.charAt(position));
+          position++;
+        }
+        tokens.add(new CSVToken(currentToken.toString(), CSVTokenType.NUMBER));
+        currentToken.setLength(0);
+        continue;
+      }
+      currentToken.append(currentChar);
+      currentType = CSVTokenType.STRING;
+      position++;
+    }
+    if (currentToken.length() > 0) {
+      tokens.add(createToken(currentToken.toString(), currentType));
+    }
+    return tokens;
+  }
+  private CSVToken createToken(String value, CSVTokenType type) {
+    if (type == CSVTokenType.STRING && value.matches("-?\\d+(\\.\\d+)?")) {
+      return new CSVToken(value, CSVTokenType.NUMBER);
+    }
+    return new CSVToken(value, type);
   }
 }
