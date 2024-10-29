9c45e9a47bd7534c29e269f777f74cf4db3e395d
Limehello <plxsmazis@gmail.com>
2024-10-29 19:27:41 +0800

    Update CSVToken.java

--- src/main/java/com/limehello/jecko/type/text/csv/CSVToken.java
+++ src/main/java/com/limehello/jecko/type/text/csv/CSVToken.java
@@ -3,7 +3,7 @@ package com.limehello.jecko.type.text.csv;
 import com.limehello.jecko.type.text.csv.CSVTokenType;
 
 public class CSVToken {
-  private String value
+  private String value;
   private CSVTokenType type;
   public CSVToken(String value, CSVTokenType type) {
     this.value = value;
