06d9388bc78566b33cb9c33a6da5373057552160
Limehello <plxsmazis@gmail.com>
2024-10-29 20:08:23 +0800

    Update CSVLexer.java

--- src/main/java/com/limehello/jecko/type/text/csv/CSVLexer.java
+++ src/main/java/com/limehello/jecko/type/text/csv/CSVLexer.java
@@ -1,7 +1,14 @@
 package com.limehello.jecko.type.text.csv;
 
-import java.util
+import java.util.ArrayList;
+import java.util.List;
 
 public class CSVLexer {
-  
+  private String input;
+  private int position;
+  public CSVLexer(String input) {
+    this.input = input;
+    this.position = 0;
+  }
+  public List<CSV
 }
