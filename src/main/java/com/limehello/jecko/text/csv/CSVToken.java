5b0f0e3a66d37e8ee2056dad25fd1017d3774fe1
Limehello <plxsmazis@gmail.com>
2024-10-30 21:08:59 +0800

    Update CSVToken.java
    CSVToken is a comma-separated-value token

--- src/main/java/com/limehello/jecko/text/csv/CSVToken.java
+++ src/main/java/com/limehello/jecko/text/csv/CSVToken.java
@@ -3,20 +3,20 @@ package com.limehello.jecko.text.csv;
 import com.limehello.jecko.text.csv.CSVTokenType;
 
 public class CSVToken {
-  private char value;
-  private CSVTokenType type;
-  public CSVToken(char value, CSVTokenType type) {
-    this.value = value;
-    this.type = type;
-  }
-  public CSVTokenType getType() {
-    return CSVTokenType;
-  }
-  public String getValue() {
-    return value;
-  }
-  @Override
-  public String toString() {
-    return "CSVToken(type=" + 
-  }
+    private char value;
+    private CSVTokenType type;
+    public CSVToken(char value, CSVTokenType type) {
+        this.value = value;
+        this.type = type;
+    }
+    public CSVTokenType getType() {
+        return type;
+    }
+    public char getValue() {
+        return value;
+    }
+    @Override
+    public String toString() {
+        return "CSVToken(type=" + type.toString() + ", value=" + value + ")";
+    }
 }
