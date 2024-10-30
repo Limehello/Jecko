4d7897f53c3d45343981d3e25f1c22516c5c86a3
Limehello <plxsmazis@gmail.com>
2024-10-30 21:20:16 +0800

    Update CSVLexer.java

--- src/main/java/com/limehello/jecko/text/csv/CSVLexer.java
+++ src/main/java/com/limehello/jecko/text/csv/CSVLexer.java
@@ -1,1 +1,21 @@
+package com.limehello.jecko.text.csv;
 
+import com.limehello.jecko.text.csv.CSVToken;
+import com.limehello.jecko.text.csv.CSVTokenType;
+
+public class CSVLexer {
+  private String input;
+  private int position;
+  public CSVLexer(String input) {
+    this.input = input;
+    this.position = 0;
+  }
+  public List<String> tokenize() {
+    List<String> tokens = new ArrayList<>();
+    tokens.clear();
+    this.position = 0;
+    for (char ch : this.input.toCharArray()) {
+      if (Character.isWhitespace()
+    }
+  }
+}
