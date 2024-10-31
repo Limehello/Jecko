0a19da7a3ea796e80877d1483289d509fc9d321e
Limehello <plxsmazis@gmail.com>
2024-10-31 20:09:14 +0800

    Create TXT.java

---
+++ src/main/java/com/limehello/jecko/text/txt/TXT.java
@@ -0,0 +1,11 @@
+package com.limehello.jecko.text.txt.TXTProcessor;
+
+public class TXT {
+  private String input;
+  public TXT(String input) {
+    this.input = input;
+  }
+  public String process() {
+    return input.toString();
+  }
+}
