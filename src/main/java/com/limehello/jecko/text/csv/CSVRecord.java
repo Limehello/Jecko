064dbb7bd568bee651e11eef991531bd4d6387e6
Limehello <plxsmazis@gmail.com>
2024-10-31 23:44:41 +0800

    Update CSVRecord.java

--- src/main/java/com/limehello/jecko/text/csv/CSVRecord.java
+++ src/main/java/com/limehello/jecko/text/csv/CSVRecord.java
@@ -1,6 +1,12 @@
 package com.limehello.jecko.text.csv;
 
+import com.limehello.jecko.text.csv.CSVToken;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CSVRecord {
-  public CSVRecord() {
+  private List<CSVToken> tokens;
+  public CSVRecord(ArrayList<CSVToken> tokens) {
+    this.tokens = tokens;
   }
 }
