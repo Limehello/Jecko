65d6941d6db4dd916a01001cfa0804470dd1d9a0
Limehello <plxsmazis@gmail.com>
2024-10-31 22:42:56 +0800

    Update CSVException.java

--- src/main/java/com/limehello/jecko/text/csv/CSVException.java
+++ src/main/java/com/limehello/jecko/text/csv/CSVException.java
@@ -4,16 +4,24 @@ import com.limehello.jecko.text.csv.CSVExceptionType;
 import java.io.IOException;
 
 public class CSVException extends IOException {
-  public CSVException(CSVExceptionType errorType, int position) {
-    super(buildMessage(errorType, position));
+  private int position;
+  public CSVException(CSVExceptionType errorType, String defaultMsg, int position) {
+    this.position = position;
+    super(buildMessage(errorType, position, defaultMsg));
   }
-  public String buildMessage(CSVExceptionType type, int position) {
+  public String buildMessage(CSVExceptionType type, int position, String defaultMsg) {
     switch (type) {
       case "UNCLOSED_QUOTES":
         return "Unclosed quotes at position: " + position;
       case "SPACES_BETWEEN":
         return "Spaces within unquoted value at position: " + position;
-      case "
+      case "INVALID_CHARACTER"
+        return "Invalid character from unavailable charset at position: " + position;
+      default:
+        return defaultMsg;
     }
+  }
+  public int getPosition() {
+    return position;
   }
 }
